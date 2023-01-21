47f42ac358c6d49d107acb2d9ad931f0c2e02ba4
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2023-01-22 00:26:42 +0700

    Progress - Workbox

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '0b4e3085486f2efca2149447c0ac19a4',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
